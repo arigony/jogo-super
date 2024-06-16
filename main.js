28b14b2d5c747856b349354ec8d57ec65cf8f72c
arigony <42611942+arigony@users.noreply.github.com>
2024-06-16 08:12:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,19 @@ var swfobject = {};
 swfobject.embedSWF = function(url, cont, width, height){
     var ruffle = window.RufflePlayer.newest(),
         player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
-            width: width,
-            height: height,
-            style: 'width: ' + width + 'px; height: ' + height + 'px',
+            width: '100%',
+            height: '100%',
+            style: 'width: 100%; height: 100%;',
         });
 
     player.load({ url: url });
 }
 
+// Adjust game size on window resize
+window.addEventListener('resize', function() {
+    var ruffleElement = document.getElementById('ruffle');
+    if (ruffleElement) {
+        ruffleElement.style.width = '100%';
+        ruffleElement.style.height = '100%';
+    }
+});
