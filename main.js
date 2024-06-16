4dfa4c36018fee6fb6480c818a694aeb467c1988
arigony <42611942+arigony@users.noreply.github.com>
2024-06-16 08:00:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,13 @@
 var swfobject = {};
 
-swfobject.embedSWF = function(url, cont, width, height) {
+swfobject.embedSWF = function(url, cont, width, height){
     var ruffle = window.RufflePlayer.newest(),
-        container = document.getElementById(cont),
-        player = Object.assign(container.appendChild(ruffle.createPlayer()), {
-            style: 'width: 100%; height: 100%'
+        player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
+            width: width,
+            height: height,
+            style: 'width: ' + width + 'px; height: ' + height + 'px',
         });
 
     player.load({ url: url });
+}
 
-    function resizePlayer() {
-        var aspectRatio = 4 / 3;  // Proporção do jogo
-        var newWidth = container.clientWidth;
-        var newHeight = newWidth / aspectRatio;
-        
-        // Ajustar a altura do contêiner com base na proporção
-        container.style.height = newHeight + 'px';
-        player.style.width = newWidth + 'px';
-        player.style.height = newHeight + 'px';
-    }
-
-    // Ajustar o tamanho do player quando a janela é redimensionada ou a orientação muda
-    window.addEventListener('resize', resizePlayer);
-    window.addEventListener('orientationchange', resizePlayer);
-
-    // Chamar a função inicialmente para ajustar o tamanho
-    resizePlayer();
-};
